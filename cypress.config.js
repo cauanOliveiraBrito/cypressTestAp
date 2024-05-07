59b2aebf1cb8ddf4fca1a89ee7c6f17d32f64a59
cauanOliveiraBrito <cauano112@gmail.com>
2024-05-07 11:40:00 -0300

    Adiciona testes Cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'o2j3ir',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    chromeWebSecurity: false
+  },
+  "reporter": "cypress-mochawesome-reporter"
+}
+
+);
